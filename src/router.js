09778726fed2dc1f6a4019b1bc026c5712982b9d
Hillel Familant <hillel.familant@macys.com>
2019-08-08 21:41:22 -0700

    setting up vue and basic routing

---
+++ src/router.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Ping from './components/Ping.vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld,
+    },
+		{
+      path: '/ping',
+      name: 'Ping',
+      component: Ping,
+    }
+  ],
+});
