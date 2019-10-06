2075c72c9c0cc049418001c50a5aaa79446f0d8c
Hillel Familant <hillel.familant@macys.com>
2019-10-06 00:10:28 -0700

    Add update functionality and PUT on backend

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -26,7 +26,12 @@
               </td>
               <td>
                 <div class="btn-group" role="group">
-                  <button type="button" class="btn btn-warning btn-sm">Update</button>
+                  <button 
+									    type="button" 
+											class="btn btn-warning btn-sm" 
+											@click="editBook(book)">
+											Update
+									</button>
                   <button type="button" class="btn btn-danger btn-sm">Delete</button>
                 </div>
               </td>
@@ -71,6 +76,43 @@
   </b-form>
 </b-modal>
 
+<b-modal ref="editBookModal"
+         id="book-update-modal"
+         title="Update"
+         hide-footer>
+  <b-form @submit="onSubmitUpdate" @reset="onResetUpdate" class="w-100">
+  <b-form-group id="form-title-edit-group"
+                label="Title:"
+                label-for="form-title-edit-input">
+      <b-form-input id="form-title-edit-input"
+                    type="text"
+                    v-model="editForm.title"
+                    required
+                    placeholder="Enter title">
+      </b-form-input>
+    </b-form-group>
+    <b-form-group id="form-author-edit-group"
+                  label="Author:"
+                  label-for="form-author-edit-input">
+        <b-form-input id="form-author-edit-input"
+                      type="text"
+                      v-model="editForm.author"
+                      required
+                      placeholder="Enter author">
+        </b-form-input>
+      </b-form-group>
+    <b-form-group id="form-read-edit-group">
+      <b-form-checkbox-group v-model="editForm.read" id="form-checks">
+        <b-form-checkbox value="true">Read?</b-form-checkbox>
+      </b-form-checkbox-group>
+    </b-form-group>
+    <b-button-group>
+      <b-button type="submit" variant="primary">Update</b-button>
+      <b-button type="reset" variant="danger">Cancel</b-button>
+    </b-button-group>
+  </b-form>
+</b-modal>
+
   </div>
 </template>
 
@@ -89,6 +131,12 @@ export default {
 			},
 			message: '',
 			showMessage: false,
+			editForm: {
+				id: '',
+				title: '',
+				author: '',
+				read: [],
+			},
 		}
 	},
 	components: {
@@ -124,6 +172,10 @@ export default {
 			this.addBookForm.title = '';
 			this.addBookForm.author = '';
 			this.addBookForm.read = [];
+			this.editForm.id = '';
+			this.editForm.name = '';
+			this.editForm.author = '';
+			this.editForm.read = [];
 		},
 		onSubmit(evt) {
 			evt.preventDefault();
@@ -138,10 +190,45 @@ export default {
 			this.addBook(payload);
 			this.initForm();
 		},
+		editBook(book) {
+			this.editForm = book;
+		},
+		onSubmitUpdate(evt) {
+			evt.preventDefault();
+			this.$refs.editBookModal.hide();
+			let read = false;
+			if (this.editForm.read[0]) read = true;
+			const payload = {
+				title: this.editForm.title,
+				author: this.editForm.author,
+				read,
+			};
+			this.updateBook(payload, this.editForm.id);
+		},
+		updateBook(payload, bookID) {
+			const path = `http://localhost:5000/books/${bookID}`;
+			axios.put(path, payload)
+			  .then(() => {
+					this.getBooks();
+					this.message = 'Book updated!';
+					this.showMessage = true;
+				})
+				.catch((error) => {
+					// eslint-disable-next-line
+					console.error(error);
+					this.getBooks();
+				})
+		},
 		onReset(evt) {
 			evt.preventDefault();
 			this.$refs.addBookModal.hide();
 			this.initform();
+		},
+		onResetUpdate(evt) {
+			evt.preventDefault();
+			this.$refs.editBookModal.hide();
+			this.initForm();
+			this.getBooks();
 		},
 	},
 	created() {
--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import uuid
 
 # config
 DEBUG = True
@@ -14,16 +15,19 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 # add books 
 BOOKS = [
   {
+    'id': uuid.uuid4(),
     'title': 'On the Road',
     'author': 'Jack Kerouac',
     'read': True
   },
   {
+    'id': uuid.uuid4(),	
     'title': 'Harry Potter and the Philopher\'s Stone',
     'author': 'J. K. Rowling',
     'read': False
   },
   {
+    'id': uuid.uuid4(),
     'title': 'Green Eggs and Ham',
     'author': 'Dr. Seuss',
     'read': True
@@ -45,6 +49,7 @@ def all_books():
     if request.method == 'POST':
         post_data = request.get_json()
         BOOKS.append({
+            'id': uuid.uuid4(),
             'title': post_data.get('title'),
             'author': post_data.get('author'),
             'read': post_data.get('read')
@@ -53,6 +58,28 @@ def all_books():
     else:
         response_object['books'] = BOOKS
     return jsonify(response_object)
+
+@app.route('/books/<book_id>', methods=['PUT'])
+def single_book(book_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        remove_book(book_id)
+        BOOKS.append({
+            'id': uuid.uuid4(),
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read'),
+        })
+        response_object['message'] = 'Books updated!'
+    return jsonify(response_object)
+
+def remove_book(book_id):
+    for book in BOOKS:
+        if book['id'] == book_id:
+            BOOKS.remove(book)
+            return True
+        return False
 
 if __name__ == '__main__':
   app.run()
